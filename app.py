aa71d71ae5aa01ea8ea71914200f392247919371
T-Lewand <61983571+T-Lewand@users.noreply.github.com>
2021-11-29 20:40:40 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -15,122 +15,57 @@ app = dash.Dash(__name__)
 # ------------------------------------------
 
 dataset = Dataset()
-#rover1_data = dataset.read_data('20211006', '1')
-dates = dataset.get_dates(as_dict=True)
-rover1_data = dataset.read_data('20211006', '1')
 
+dates = dataset.get_dates()
+data = dataset.read('20211127', 'main')
+
+parameters_dict = [{'label': 'Temperatura', 'value': 'Temperature'},
+                    {'label': 'Wilgotność', 'value': 'RH'},
+                    {'label': 'Pm 1.0', 'value': 'Pm1.0'},
+                    {'label': 'Pm 2.5', 'value': 'Pm2.5'},
+                    {'label': 'Pm 10', 'value': 'Pm10'},
+                    {'label': 'Formaldehyd', 'value': 'HCHO'}]
+graphs_id = [1,2,3,4,5,6]
+graphs_id1 = [1, 2, 3]
+graphs_id2 = [4, 5, 6]
 # -------------------------------------------
 
 app.layout = html.Div(children=[
     html.H1(children='Sensorowo-roWeRowO'),
 
     html.Div(children=[
-        dcc.Dropdown(id='rover_selector',
-                     options=[{'label': 'Rower 1', 'value': '1'},
-                              {'label': 'Rower 2', 'value': '2'}],
-                     value='1', multi=True),
-        dcc.Dropdown(id='date_selector',
-                     options=dates,
-                     value='20211006'),
-
-        dcc.Dropdown(id='data_selector_1',
-                     options=[{'label': 'Temperatura', 'value': 'temp'},
-                              {'label': 'Ciśnienie', 'value': 'pressure'},
-                              {'label': 'Wilgotność', 'value': 'humidity'},
-                              {'label': 'Pmy1', 'value': 'pmy1'},
-                              {'label': 'Pmy2', 'value': 'pmy2'},
-                              {'label': 'Pmy3', 'value': 'pmy3'},
-                              {'label': 'Pmy4', 'value': 'pmy4'},
-                              {'label': 'Pmy5', 'value': 'pmy5'}]),
-        html.Br(),
-        dcc.Graph(id='graph_1', figure={})
-    ], style={'width': '49%', 'display': 'inline-block'}),
-
-    html.Div(children=[
-        dcc.Dropdown(id='data_selector_2',
-                     options=[{'label': 'Temperatura', 'value': 'temp'},
-                              {'label': 'Ciśnienie', 'value': 'pressure'},
-                              {'label': 'Wilgotność', 'value': 'humidity'},
-                              {'label': 'Pmy1', 'value': 'pmy1'},
-                              {'label': 'Pmy2', 'value': 'pmy2'},
-                              {'label': 'Pmy3', 'value': 'pmy3'},
-                              {'label': 'Pmy4', 'value': 'pmy4'},
-                              {'label': 'Pmy5', 'value': 'pmy5'}]),
-        html.Br(),
-        dcc.Graph(id='graph_2', figure={})
-    ], style={'width': '49%', 'display': 'inline-block'}),
-
-    html.Div(children=[
-        dcc.Dropdown(id='data_selector_3',
-                     options=[{'label': 'Temperatura', 'value': 'temp'},
-                              {'label': 'Ciśnienie', 'value': 'pressure'},
-                              {'label': 'Wilgotność', 'value': 'humidity'},
-                              {'label': 'Pmy1', 'value': 'pmy1'},
-                              {'label': 'Pmy2', 'value': 'pmy2'},
-                              {'label': 'Pmy3', 'value': 'pmy3'},
-                              {'label': 'Pmy4', 'value': 'pmy4'},
-                              {'label': 'Pmy5', 'value': 'pmy5'}]),
-        html.Br(),
-        dcc.Graph(id='map', figure={})], style={'width': '50%', 'display': 'inline-block', 'padding-left': '25%'})
-    ])
-
-@app.callback(
-    Output(component_id='graph_1', component_property='figure'),
-    [Input(component_id='data_selector_1', component_property='value'),
-     Input(component_id='rover_selector', component_property='value'),
-     Input(component_id='date_selector', component_property='value')]
-)
-def func(parameter, rover, date):
-    if isinstance(rover, list):
-        data = dataset.read_data(date, rover[0])
-    else:
-        data = dataset.read_data(date, rover)
-
-    graph = px.line(data_frame=data, x='time', y=parameter)
-    return graph
+            dcc.Dropdown(id='date_selector',
+                        options=dates,
+                        value='20211006'),
+            dcc.Input(id="start_time", type="text", placeholder="HH:MM:SS", value=None),
+            dcc.Input(id="end_time", type="text", placeholder="HH:MM:SS", value=None),
+            html.Br()]),
+    html.Div(children=[dcc.Dropdown(id='data_selector_{}'.format(i), options=parameters_dict) for i in graphs_id1],
+             style={'width':"50%", 'display': 'inline-block'}),
+    html.Div(children=[dcc.Dropdown(id='data_selector_{}'.format(i), options=parameters_dict) for i in graphs_id2],
+             style={'width':"50%", 'display': 'inline-block'}),
+    html.Br(),
+    html.Div(children=[dcc.Graph(id='graph_{}'.format(i), figure={}) for i in graphs_id1],
+             style={'width':"50%", 'display': 'inline-block'}),
+    html.Div(children=[dcc.Graph(id='graph_{}'.format(i), figure={}) for i in graphs_id2],
+             style={'width':"50%", 'display': 'inline-block'})
+            ])
 
 
-@app.callback(
-    Output(component_id='graph_2', component_property='figure'),
-    [Input(component_id='data_selector_2', component_property='value'),
-     Input(component_id='rover_selector', component_property='value'),
-     Input(component_id='date_selector', component_property='value')]
-)
-def func(parameter, rover, date):
-    if isinstance(rover, list):
-        data = dataset.read_data(date, rover[0])
-    else:
-        data = dataset.read_data(date, rover)
 
-    graph = px.line(data_frame=data, x='time', y=parameter)
-    return graph
+for i in graphs_id:
+    @app.callback(
+        Output(component_id='graph_{}'.format(i), component_property='figure'),
+        [Input(component_id='data_selector_{}'.format(i), component_property='value'),
+         Input(component_id='date_selector', component_property='value'),
+         Input(component_id='start_time', component_property='value'),
+         Input(component_id='end_time', component_property='value')]
+    )
+    def func(parameter, date, start_time, end_time):
+        data = dataset.read(date, start_time=start_time, end_time=end_time)
 
-@app.callback(
-    Output(component_id='map', component_property='figure'),
-    [Input(component_id='data_selector_3', component_property='value'),
-     Input(component_id='rover_selector', component_property='value'),
-     Input(component_id='date_selector', component_property='value')]
-)
-def func_map(parameter, rover, date):
-    if isinstance(rover, list):
-        print('----------')
-        data = dataset.read_data(date, rover[0])
-
-        for i in range(len(rover)):
-            try:
-                data = pd.concat([data, dataset.read_data(date, rover[i+1])], axis=0)
-            except:
-                pass
-    else:
-        print(rover)
-        print(isinstance(rover, list))
-        data = dataset.read_data(date, rover)
-
-    hex_map = ff.create_hexbin_mapbox(data_frame=data, lat='B', lon='L', color=parameter,
-                                      labels={'color': str(parameter)},
-                                      nx_hexagon=20, opacity=0.9, color_continuous_scale='sunsetdark',
-                                      mapbox_style='open-street-map')
-    return hex_map
+        graph = px.line(data_frame=data, x='Time', y=parameter)
+        return graph
 
 
 if __name__ == '__main__':
